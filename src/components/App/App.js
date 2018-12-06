3f4f4be07f5f013c486ac7dd765a2de9ef8e414a
KarlaJeanNelson <karla.jean.nelson@gmail.com>
2018-12-06 13:47:39 -0600

    added dependencies needed for styling components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './App.theme';
-// import logo from './logo.svg';
-// import './App.css';
-import AddKoalaForm from '../AddKoalaForm/AddKoalaForm';
+import KoalaForm from '../AddKoalaForm/KoalaForm';
 import KoalaList from '../KoalaList/KoalaList';
 
 class App extends Component {
@@ -12,8 +10,8 @@ class App extends Component {
     return (
       <MuiThemeProvider theme={theme}>
 				<CssBaseline />
-        <AddKoalaForm />
-        <KoalaList />
+				<KoalaForm />
+				<KoalaList />
       </MuiThemeProvider>
     );
   }
