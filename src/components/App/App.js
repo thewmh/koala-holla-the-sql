4fe55c91ad8e200090d89802907597a1f4fb627f
William Harrison <wm.harrison@gmail.com>
2018-12-06 13:32:50 -0600

    committing nav changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 import AddKoalaForm from '../AddKoalaForm/AddKoalaForm';
 import KoalaList from '../KoalaList/KoalaList';
+import Nav from '../Nav/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav/>
         <AddKoalaForm/>
         <KoalaList/>
       </div>
