dc6cb7d58b0ed6f22b927b1fbaeaa0763a08a14c
William Harrison <wm.harrison@gmail.com>
2018-12-06 11:29:12 -0600

    working on form to add koala prisoners to the database

--- src/components/AddKoalaForm/AddKoalaForm.js
+++ src/components/AddKoalaForm/AddKoalaForm.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
 class AddKoalaForm extends Component {
+
+    state = {
+            name: '',
+            gender: 'f',
+            age: 0,
+            rtt: false,
+            notes: '',    
+    }
+
+    // capture input values and store them locally until ready to send prisoner data
+    handleChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // onClick event to 'save' the data, result is imprisoned koala
+    imprisonTheKoala = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+        // this.props.dispatch({type: 'ADD_RECIPE_DESCRIPTION', payload: this.state});
+        this.setState({newKoala: {
+            name: '',
+            gender: 'f',
+            age: 0,
+            rtt: false,
+            notes: '',
+        }});
+        }
+
     render() {
         return (
             <div>
-                <h1>This is a Koala form</h1>
+                <input required onChange={this.handleChange} type="text" placeholder="Enter the koala's name" name="name"></input>
+                <select required onChange={this.handleChange} placeholder="Enter the koala's gender" name="gender">
+                <option value="f">F</option>
+                <option value="m">M</option>
+                </select>
+                <input required onChange={this.handleChange} type="number" placeholder="Enter the koala's age" name="age"></input>
+                <select required onChange={this.handleChange} placeholder="Enter the koala's ready to transfer status" name="ready-to-transfer">
+                <option value="false">False</option>
+                <option value="true">True</option>
+                </select>
+                <textarea required onChange={this.handleChange} type="text" placeholder="Enter any notes about the koala" name="notes"></textarea>
+                <button onClick={this.imprisonTheKoala}>Imprison the Koala</button>
             </div>
         );
     }
